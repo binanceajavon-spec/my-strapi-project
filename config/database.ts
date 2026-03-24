deea191a8e3359f0f16d2b81049b7623f92a81a8
Ajavon <binanceajavon@gmail.com>
2026-03-24 02:08:49 +0100

    fix: clean postgres config for internal network

--- config/database.ts
+++ config/database.ts
@@ -1,30 +1,9 @@
-import path from 'path';
-
-export default ({ env }) => {
-  const databaseUrl = env('DATABASE_URL');
-
-  // Si on est sur Railway (DATABASE_URL existe)
-  if (databaseUrl) {
-    return {
-      connection: {
-        client: 'postgres',
-        connection: {
-          connectionString: databaseUrl,
-          ssl: { rejectUnauthorized: false },
-        },
-        pool: { min: 2, max: 10 },
-      },
-    };
-  }
-
-  // Si on est en local sur ton Windows (DATABASE_URL n'existe pas)
-  return {
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'sqlite',
-      connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
+      connectionString: env('DATABASE_URL'),
     },
-  };
-};
+    pool: { min: 2, max: 10 },
+  },
+});
