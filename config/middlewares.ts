98a577f4fa8866c76a597ce4d6d9a9ca1ed18ad6
Ajavon <binanceajavon@gmail.com>
2026-03-28 12:43:02 +0100

    Fix: Remove duplicate export and enable wildcard CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -7,8 +7,6 @@ export default [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:'],
           upgradeInsecureRequests: null,
         },
       },
@@ -17,44 +15,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      // Note : On ne met SURTOUT PAS "enabled: true" ici pour Strapi 5
-      origin: [
-        'https://binanceajavon-spec.github.io',
-        'https://binanceajavon-spec.github.io/loji-premiere',
-        'https://binanceajavon-spec.github.io/loji-premiere/',
-        'http://localhost:1337'
-      ],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeaderOnError: true,
-    },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
-
-export default [
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['*'], // ON AUTORISE TOUT LE MONDE ICI
+      origin: ['*'], 
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
