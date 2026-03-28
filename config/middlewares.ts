7d5651698409a8ce5c3dee263d4d42ec7eb2de1b
Ajavon <binanceajavon@gmail.com>
2026-03-28 11:48:47 +0100

    Fix: CORS for loji-premiere

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,13 +1,27 @@
-module.exports = [
+export default [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
+      // Suppression de "enabled: true" car Strapi 5 ne le supporte plus ici
       origin: [
         'https://binanceajavon-spec.github.io',
-        'http://localhost:3000',
-        'http://localhost:1337'
+        'https://binanceajavon-spec.github.io/loji-premiere',
+        'https://binanceajavon-spec.github.io/loji-premiere/',
+        'http://localhost:1337',
+        'http://localhost:3000'
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
