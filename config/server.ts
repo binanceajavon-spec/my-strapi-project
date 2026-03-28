ab54d3463ca4d861c3e61094a34c1d464c9e1e0a
Ajavon <binanceajavon@gmail.com>
2026-03-28 22:01:49 +0100

    fix: proxy true for railway

--- config/server.ts
+++ config/server.ts
@@ -1,14 +1,17 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // On force les headers ici car le middleware CORS semble défaillant
+
+  proxy: true, // 🔥 FIX CRITIQUE
+
   settings: {
     cors: {
       enabled: true,
-      origin: ['https://binanceajavon-spec.github.io', 'http://localhost:1337'],
+      origin: ['https://binanceajavon-spec.github.io'],
       headers: ['*'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
     },
