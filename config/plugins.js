89285dee566be86da665f49e116d7dc8e9db0a36
Ajavon <binanceajavon@gmail.com>
2026-03-29 04:03:17 +0100

    Force Cloudinary upload provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,24 @@
 module.exports = ({ env }) => ({
-  // Activation du plugin Users & Permissions (obligatoire pour l'auth)
+  // Activation du plugin Users & Permissions
   'users-permissions': {
     enabled: true,
     config: {
       jwtSecret: env('JWT_SECRET'),
     },
   },
-  // Cloudinary pour les images
+  // Configuration Cloudinary pour l'upload
   upload: {
+    enabled: true,
     config: {
       provider: 'cloudinary',
       providerOptions: {
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
   },
