da2e12063a7d57758770e93716139f2720a2c31d
Ajavon <binanceajavon@gmail.com>
2026-03-29 05:04:30 +0100

    FIX FINAL: Admin + Cloudinary

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: '0.0.0.0',
+  port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://my-strapi-project-production-d4d2.up.railway.app'),
+  proxy: true,
+  app: {
+    keys: env.array('APP_KEYS', ['key1', 'key2', 'key3']),
+  },
+});
